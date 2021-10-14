1cc8c56badb0a358d28695b7b90924cffd96f31b
jack-cloud-platform <38191888+jack-cloud-platform@users.noreply.github.com>
2021-10-14 14:50:55 +0100

    Changed ping endpoint URLS from /health to /ping (#24)

--- src/SFA.DAS.DownloadService.Api/Startup.cs
+++ src/SFA.DAS.DownloadService.Api/Startup.cs
@@ -108,7 +108,7 @@ namespace SFA.DAS.DownloadService.Api
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseSession();
-            app.UseHealthChecks("/health");
+            app.UseHealthChecks("/ping");
             app.UseRequestLocalization();
 
 
