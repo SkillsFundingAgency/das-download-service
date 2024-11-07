d99ba4de882f2ea8a7af9049e5c8490a247c2ed6
Chris Woodcock <chris@humesoftware.com>
2024-11-07 15:44:37 +0000

    Removed unused api authentication

--- src/SFA.DAS.DownloadService.Settings/IWebConfiguration.cs
+++ src/SFA.DAS.DownloadService.Settings/IWebConfiguration.cs
@@ -4,7 +4,6 @@ namespace SFA.DAS.DownloadService.Settings
 {
     public interface IWebConfiguration
     {
-        ApiAuthentication ApiAuthentication { get; set; }
         ManagedIdentityApiAuthentication RoatpApiAuthentication { get; set; }
         string RedisConnectionString { get; set; }
         string DataProtectionKeysDatabase { get; set; }
@@ -12,7 +11,6 @@ namespace SFA.DAS.DownloadService.Settings
 
     public class WebConfiguration : IWebConfiguration
     {
-        [JsonRequired] public ApiAuthentication ApiAuthentication { get; set; }
         [JsonRequired] public ManagedIdentityApiAuthentication RoatpApiAuthentication { get; set; }
         [JsonRequired] public string RedisConnectionString { get; set; }
         [JsonRequired] public string DataProtectionKeysDatabase { get; set; }
