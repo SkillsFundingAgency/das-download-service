6ff4a080d8e640cff8e9ab5b8a85b4bf7389764e
Afzal Hussain <afi259@hotmail.co.uk>
2025-11-24 17:07:24 +0000

    CSP-2323 updated nested blocks

--- src/SFA.DAS.DownloadService.Api/Infrastructure/DataProtectionStartupExtensions.cs
+++ src/SFA.DAS.DownloadService.Api/Infrastructure/DataProtectionStartupExtensions.cs
@@ -14,21 +14,18 @@ public static class DataProtectionStartupExtensions
 {
     public static IServiceCollection AddDataProtection(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment)
     {
-        if (environment.IsDevelopment() || configuration == null)
+        if (!environment.IsDevelopment() && configuration != null)
         {
-            return services;
+            var redisConnectionString = configuration.GetSection("RedisConnectionString").Get<string>();
+            var dataProtectionKeysDatabase = configuration.GetSection("DataProtectionKeysDatabase").Get<string>();
+
+            var redis = ConnectionMultiplexer
+                .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
+
+            services.AddDataProtection()
+                .SetApplicationName("das-download-service")
+                .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
         }
-
-        var redisConnectionString = configuration.GetSection("RedisConnectionString").Get<string>();
-        var dataProtectionKeysDatabase = configuration.GetSection("DataProtectionKeysDatabase").Get<string>();
-
-        var redis = ConnectionMultiplexer
-            .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
-
-        services.AddDataProtection()
-            .SetApplicationName("das-download-service")
-            .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
-
         return services;
     }
 }
